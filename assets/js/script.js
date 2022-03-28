733f0ebfd54749a05c0cdd90e575ea6d174fd5bb
Helen Goatly <hgoatly@hotmail.com>
2022-03-28 22:52:45 +0100

    Add logic to repertoire, and display it in tables within accordions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,25 @@ for (var item in data) {
 console.log(repertoireList);
 
 
-data.forEach(function(obj) { console.log(obj.title); });
+data.forEach(function(obj) { console.log(obj.title);
+    
+});
+
+const classical = document.querySelector('#classical');
+
+data.forEach(item => {
+    if (item.genre === "classical") {
+  classical.innerHTML += `
+  
+  <tr>
+    <td>
+        ${item.title}
+    </td>
+    <td>
+    ${item.composer}
+    </td>
+    </tr>
+
+  `};
+});
+
